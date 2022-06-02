d822083a5d50f7853953e5eaeaf92d3cfda936f7
tejask432 <tejask432@gmail.com>
2022-06-02 14:50:18 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import React,{ useState } from "react";
+const App = () => {
+    const [result, setResult]= useState(0);
+    return(
+        <>
+            <div className="container">
+                <form>
+                    <input type="text" value={result}/>
+                </form> 
+                <div className="keypad">
+                    <button>MC</button>
+                    <button>MR</button>
+                    <button>M+</button>
+                    <button>M-</button>
+                    <button>MS</button>
+                    <button>M<sup><i class="fa-solid fa-caret-down"></i></sup></button>
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+                    <button>%</button>
+                    <button><span>&#8730;</span></button>
+                    <button>x<sup>2</sup></button>
+                    <button><sup>1</sup>/<sub></sub>x</button>
+
+                    <button>CE</button>
+                    <button>C</button>
+                    <button><i class="fa-solid fa-delete-left"></i></button>
+                    <button><i class="fa-solid fa-divide"></i></button>
+                    
+                    <button>7</button>
+                    <button>8</button>
+                    <button>9</button>
+                    <button><i class="fa-solid fa-x"></i></button>
+                    
+                    <button>4</button>
+                    <button>5</button>
+                    <button>6</button>
+                    <button><i class="fa-solid fa-minus"></i></button>
+                    
+                    <button>1</button>
+                    <button>2</button>
+                    <button>3</button>
+                    <button><i class="fa-solid fa-plus"></i></button>
+                    
+                    <button><i class="fa-solid fa-plus-minus"></i></button>
+                    <button>0</button>
+                    <button><b>.</b></button>
+                    <button><i class="fa-solid fa-equals"></i></button>
+
+                </div>
+            </div>
+        </>
+    );
+};
 
 export default App;
